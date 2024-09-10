d8effb1762bd2c2b1d1c1505da75c38c893c893d
Irvanscx <irvanmaulana292@gmail.com>
2024-09-10 14:59:19 +0700

    add

--- app/dashboard/sites/page.tsx
+++ app/dashboard/sites/page.tsx
@@ -8,12 +8,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { PlusCircleIcon } from "lucide-react";
+import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import defaultImageUrl from "@/public/default.png";
+import { redirect } from "next/navigation";
+import Defaultimage from "@/public/default.png";
 import { EmptyState } from "@/app/components/dashboard/EmptyState";
+
 async function getData(userId: string) {
   const data = await prisma.site.findMany({
     where: {
@@ -23,53 +24,59 @@ async function getData(userId: string) {
       createdAt: "desc",
     },
   });
+
   return data;
 }
-export default async function SitesRoutes() {
-  const { getUser } = await getKindeServerSession();
+
+export default async function SitesRoute() {
+  const { getUser } = getKindeServerSession();
   const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
   const data = await getData(user.id);
   return (
     <>
       <div className="flex w-full justify-end">
         <Button asChild>
-          <Link href="/dashboard/sites/new">
-            <PlusCircleIcon className="size-4 mr-2 " />
-            Create Site
+          <Link href={"/dashboard/sites/new"}>
+            <PlusCircle className="mr-2 size-4" /> Create Site
           </Link>
         </Button>
       </div>
 
       {data === undefined || data.length === 0 ? (
         <EmptyState
-          title="No sites created yet"
-          description="You currently don't have any sites. Please create some so that
-       you can start tracking your traffic."
+          title="You dont have any Sites created"
+          description="You currently dont have any Sites. Please create some so that you can
+        see them right here!"
           buttonText="Create Site"
           href="/dashboard/sites/new"
         />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-7">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
           {data.map((item) => (
             <Card key={item.id}>
               <Image
-                src={item.imageUrl ?? defaultImageUrl}
+                src={item.imageUrl ?? Defaultimage}
                 alt={item.name}
                 className="rounded-t-lg object-cover w-full h-[200px]"
                 width={400}
                 height={200}
               />
-              <CardHeader className="flex flex-col justify-center items-center">
-                <CardTitle className="truncate overflow-hidden whitespace-nowrap text-overflow-ellipsis w-full">
-                  <Link href={`/dashboard/sites/${item.id}`}>{item.name}</Link>
-                </CardTitle>
+              <CardHeader>
+                <CardTitle className="truncate">{item.name}</CardTitle>
                 <CardDescription className="line-clamp-3">
                   {item.description}
                 </CardDescription>
               </CardHeader>
+
               <CardFooter>
                 <Button asChild className="w-full">
-                  <Link href={`/dashboard/sites/${item.id}`}>View Article</Link>
+                  <Link href={`/dashboard/sites/${item.id}`}>
+                    View Articles
+                  </Link>
                 </Button>
               </CardFooter>
             </Card>
